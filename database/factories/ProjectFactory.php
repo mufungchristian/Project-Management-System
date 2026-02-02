b85588df88e89874461395a61517053d244f8008
chrisdev <fongohmufungchristian@gmail.com>
2026-02-02 01:34:38 +0100

    Add files via upload

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Client;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user = User::factory()->create();
+        $startDate = $this->faker->dateTimeBetween('-6 months', 'now');
+        $endDate = $this->faker->dateTimeBetween($startDate, '+1 year');
+        
+        return [
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'image_path' => $this->faker->optional()->imageUrl(640, 480, 'business'),
+            'status' => $this->faker->randomElement(['in_progress', 'pending','cancelled', 'completed', 'on_hold']),
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+            'client_id' => Client::factory(),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user = User::factory()->create();
+        return [
+            'name' => $this->faker->sentence(4),
+            'description' => $this->faker->optional()->paragraph(),
+            'image_path' => $this->faker->optional()->imageUrl(640, 480, 'abstract'),
+            'status' => $this->faker->randomElement(['pending', 'in_progress', 'completed', 'cancelled','on_hold']),
+            'priority' => $this->faker->randomElement(['low', 'medium', 'high']),
+            'due_date' => $this->faker->optional()->dateTimeBetween('now', '+3 months'),
+            'assigned_user_id' => $this->faker->optional()->randomElement([User::factory(), null]),
+            'project_id' => Project::factory(),
+            'category_id' => Category::factory(),
+            'created_by' => $user->id,
+            'updated_by' => $user->id,
+        ];
+    }
+}
